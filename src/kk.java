6d8f36265a4787e9e125a6cf0507d9b6920ebbf2
rahulgangurde <rahulsgangurde26@gmail.com>
2026-04-04 15:30:18 +0530

    new file

---
+++ src/kk.java
@@ -0,0 +1,7 @@
+rahul is my name and i am a software developer. 
+i have been working in the field of software development
+ for the past 5 years. 
+ i have experience in various programming languages 
+ such as Java, Python, and C++. i am passionate about coding and always strive to improve my skills. in my free time,
+i enjoy learning new technologies and exploring different 
+  programming paradigms.
